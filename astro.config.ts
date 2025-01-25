5e8caa70f207ed5e980ca1ba1db7c7a9f1f0317d
hidenorioka <vwkr6t@gmail.com>
2025-01-26 03:02:04 +0900

    :sparkles: introduce tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 const SITE_URL = "https://hidenorioka.github.io";
 const BASE_NAME = "/site";
 
-export default defineConfig({ site: SITE_URL, base: BASE_NAME });
+export default defineConfig({
+  site: SITE_URL,
+  base: BASE_NAME,
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
