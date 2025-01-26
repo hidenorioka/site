f4c65a8a401b5fb4cdf482490755641ec0e7c981
hidenorioka <vwkr6t@gmail.com>
2025-01-26 14:03:27 +0900

    :sparkles: v1 release

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 const SITE_URL = "https://hidenorioka.github.io";
 const BASE_NAME = "/site";
@@ -7,6 +8,7 @@ const BASE_NAME = "/site";
 export default defineConfig({
   site: SITE_URL,
   base: BASE_NAME,
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
