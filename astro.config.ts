2e060c6217574ec168fae32be9e9308bd540af0f
hidenorioka <vwkr6t@gmail.com>
2025-01-27 21:14:28 +0900

    :chart_with_upwards_trend: Add Google Analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 const SITE_URL = "https://hidenorioka.github.io";
@@ -8,7 +9,14 @@ const BASE_NAME = "/site";
 export default defineConfig({
   site: SITE_URL,
   base: BASE_NAME,
-  integrations: [sitemap()],
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    sitemap(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
