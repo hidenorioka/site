275a49154435e00984f19b1a33e46f6f8edccf27
hidenorioka <vwkr6t@gmail.com>
2025-01-27 22:00:59 +0900

    :ambulance::ambulance: fix Google Analytics 2/n (#14)

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 const SITE_URL = "https://hidenorioka.github.io";
@@ -9,14 +8,7 @@ const BASE_NAME = "/site";
 export default defineConfig({
   site: SITE_URL,
   base: BASE_NAME,
-  integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    sitemap(),
-  ],
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
